802a8dbcc8a89a095eac2f946de5f51a8b97ab83
Darion Richards <darionrichards@outlook.com>
2021-09-30 14:49:31 +0100

    Created generateLower function
    Generates a lower case letter for password
    - Using ASCII table, begins from index of 97
    - Generates a random number between 1 and 26 (letters in alphabet)
    - Adds random number to 97, generating a lower case letter to be used

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
+// ASCII Table Codes - https://www.rapidtables.com/code/text/ascii-table.html
+
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
+// Generate lower case letters, starting from ASCII code 97 (a) and adding a random number between 1 and 26.
+function generateLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+}
+
 function generatePassword() {
-  // PLEASE ADD ALL YOUR CODE HERE
+    // PLEASE ADD ALL YOUR CODE HERE
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+console.log(generateLower());
