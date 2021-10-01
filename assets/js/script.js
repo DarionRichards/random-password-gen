1a2db5d540ba630f865cf5bd385894d793f7c1f0
Darion Richards <darionrichards@outlook.com>
2021-10-01 12:46:07 +0100

    Added DOM elements for JS
    INDEX.HTML
    
    - Created IDs for input options which will be used by the user
    
    - Changed IDs for a more descriptive name
    
    SCRIPT.JS
    
    - Created const variables storing each input elements of each password option
    - Changed comments for easier reading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,51 @@
 // ASCII Table Codes - https://www.rapidtables.com/code/text/ascii-table.html
 
-// Assignment Code
-var generateBtn = document.querySelector('#generate');
+/*
 
-// Generate lower case letters, starting from ASCII code 97 (a) and adding a random number between 1 and 26.
+Variable of DOM Elements
+
+*/
+
+const generateBtn = document.querySelector('#generate');
+const passwordLength = document.querySelectorAll('#length');
+const lowercaseCheck = document.querySelectorAll('#hasLowercase')
+const uppercaseCheck = document.querySelectorAll('#hasUppercase')
+const numbersCheck = document.querySelectorAll('#hasNumbers')
+const symbolsCheck = document.querySelectorAll('#hasSymbols')
+
+
+/*
+
+Create Functions
+
+*/
+
+// create lower case letters, starting from ASCII code 97 (a) and adding a random number between 1 and 26.
 function generateLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
 }
 
-// Generate upper case letters, starting from ASCII code 65 (A) and adding a random number between 1 and 26.
 function generateUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
 }
 
-// Generate random number, starting from ASCII code 48 (0) and adding a random number between 1 and 10.
 function generateNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
 }
 
-// Generate symbol, using a string of symbols and indexing a random index to select a individual random symbol
 function generateSymbols() {
     const symbols = '!"£$%^&*(){}[]@,./<>';
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
 function generatePassword() {
-    // PLEASE ADD ALL YOUR CODE HERE
+    // CODE HERE
 }
 
-// Write password to the #password input
+// write password to the #password input
 function writePassword() {
+
+    // store generated password
     var password = generatePassword();
     var passwordText = document.querySelector('#password');
 
@@ -39,4 +55,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
 
-console.log(generateSymbols());
+console.log(generateLower());
