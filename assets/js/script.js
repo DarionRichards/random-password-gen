2ecaa045e0f567d489552a3c0802605ccbd1894f
Darion Richards <darionrichards@outlook.com>
2021-10-03 16:57:41 +0100

    Added addEventListener at top of file
    - addEventListener to top of file
    - generatePassword function above functions to be called

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,36 @@ const uppercaseCheck = document.querySelector('#hasUppercase')
 const numbersCheck = document.querySelector('#hasNumbers')
 const symbolsCheck = document.querySelector('#hasSymbols')
 
+/*
+
+Click Event
+
+*/
+
+// Add event listener to check password options, and generate a password based on true options
+
+generateBtn.addEventListener('click', () => {
+    const length = passwordLength.value;
+    const hasLowercase = lowercaseCheck.checked;
+    const hasUpercase = uppercaseCheck.checked;
+    const hasNumbers = numbersCheck.checked;
+    const hasSymbols = symbolsCheck.checked;
+
+    generatePassword(length, hasLowercase, hasUpercase, hasNumbers, hasSymbols);
+
+})
 
 /*
 
 Create Functions
 
 */
+
+function generatePassword() {
+    // 1. string to store created password
+    // 2. include checked options
+    // 3. iterate through password length
+}
 
 // creates lower case letters, starting from ASCII code 97 (a) and adding a random number between 1 and 26.
 function generateLower() {
@@ -37,31 +61,6 @@ function generateSymbols() {
     const symbols = '!"£$%^&*(){}[]@,./<>';
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
-
-/*
-
-Validation Functions
-
-*/
-
-function generatePassword() {
-    // 1. string to store created password
-    // 2. include checked options
-    // 3. iterate through password length
-}
-
-// Add event listener to check password options, and generate a password based on true options
-
-generateBtn.addEventListener('click', () => {
-    const length = passwordLength.value;
-    const hasLowercase = lowercaseCheck.checked;
-    const hasUpercase = uppercaseCheck.checked;
-    const hasNumbers = numbersCheck.checked;
-    const hasSymbols = symbolsCheck.checked;
-
-    generatePassword(length, hasLowercase, hasUpercase, hasNumbers, hasSymbols);
-
-})
 
 
 // write password to the #password input
